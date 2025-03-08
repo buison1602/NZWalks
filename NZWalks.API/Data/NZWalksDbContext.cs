61b95cf4daa5e12060d32a1bc252c2d2c1767f8c
buison1602 <buicongson162@gmail.com>
2025-03-08 20:12:40 +0700

    Add project files.

---
+++ NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    // DbContext có vai trò thao tác với CSDL 
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        // DbSet là một tập hợp các đối tượng được lưu trong CSDL
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+
+    }
+}
